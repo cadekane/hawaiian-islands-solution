9d0854779259974e5ca675a488370e583ca057ef
cadekane <89505252+cadekane@users.noreply.github.com>
2024-01-13 16:12:43 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 /* Write your code here: */
 
-const islands = [
-    {
-      name: "Hawaiʻi",
-      nickname: "Big Island",
-      size: 4028,
-      popularSpots: ["Volcanoes National Park", "Mauna Kea"],
-      cities: ["Hilo", "Kailua-Kona"],
-    },
-    // Add more island objects with varying properties
-  ];
-
 /* Code to visualize your array of island objects. Do not edit */
   
   const islandsContainer = document.getElementById("islands-container");
